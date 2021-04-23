d6736a24e06e86d5f5ad08471a6914a4bf9a8234
Levi8495 <79868947+Levi8495@users.noreply.github.com>
2021-04-22 22:58:06 -0400

    Update someclassexample.py

--- someclassexample.py
+++ someclassexample.py
@@ -1,7 +1,8 @@
 import os
 import random
+import keyboard
 
-class dice_rolls():
+class diceRolls():
 
     def d6(x):
         total = 0
@@ -21,6 +22,11 @@ class dice_rolls():
             total += random.randint(1, 20)
         return total
 
+    def stat_roll():
+        number = diceRolls.d6(4)
+        while number < 8:
+            number = diceRolls.d6(4)
+        return number
 
 
 class char_stuff():
@@ -28,22 +34,22 @@ class char_stuff():
     def __init__(self):
         self.name = char_stuff.get_charName()
         self.inventory = []
-        self.hp = int(dice_rolls.d6(4)) + 15
-        self.defence = dice_rolls.d6(4)
-        self.strength = dice_rolls.d6(4)
-        self.speed = dice_rolls.d6(4)
+        self.hp = diceRolls.stat_roll() + 15
+        self.defence = diceRolls.stat_roll()
+        self.strength = diceRolls.stat_roll()
+        self.speed = diceRolls.stat_roll()
 
     def npc_maker(self, name, hp, inventory, mdef, dex, spd):
         pass
 
     def __str__(self):
         os.system('cls' if os.name == 'nt' else 'clear')
-        stats = f"************  {self.name}  ************\n"
+        stats = f"************| {self.name} |************\n"
         stats += f"\n| -  HP =  {self.hp}\n"
         stats += f"| -  DEFENCE =  {self.defence}\n"
         stats += f"| -  STRENGTH =  {self.strength}\n"
         stats += f"| -  SPEED =  {self.speed}\n"
-        stats += f"| - INVENTORY : "
+        stats += f"| -  INVENTORY : "
         for i in self.inventory:
             stats += f"{i}, "
         stats += "\n"
@@ -58,25 +64,38 @@ class char_stuff():
     def add_item(self, item):
         self.inventory.append(item)
 
-   def drop_item(self): # this seems to work
+    def drop_item(self):
         while True:
             item = input("What item do you want to drop? : ")
+            answer = input("Do you really want to drop it? Yes or No : ")
+            if answer.upper() == 'NO':
+                return
+
             try:
                 self.inventory.remove(item)
+                print("Items removed: ", item)
+                print("Press Space to continue.")
+                keyboard.wait('space')
                 break
+
             except ValueError:
                 print("You can only drop something currently in your inventory.")
                 continue
 
 
 
+def main():
+
+    someguy = char_stuff()
+    someguy.add_item("Iron Dagger")
+    someguy.add_item("Iron Chestplate")
+    someguy.add_item("Iron Leggings")
+    someguy.add_item("Iron Helmet")
+    someguy.add_item("Iron Boots")
+    print(someguy)
+    someguy.drop_item()
+    print(someguy)
 
 
-
-someguy = char_stuff()
-print(someguy)
-someguy.add_item("something cool")
-someguy.add_item("another cool thing")
-someguy.drop_item()
-print(someguy)
-k = input("lol")
+if __name__ == "__main__":
+    main()
