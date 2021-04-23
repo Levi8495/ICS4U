ec470a16565b95cc249150d5d0eebc9cd895c14e
Levi8495 <79868947+Levi8495@users.noreply.github.com>
2021-04-22 20:21:11 -0400

    Update someclassexample.py

--- someclassexample.py
+++ someclassexample.py
@@ -1,17 +1,87 @@
+import os
+import random
+
+class dice_rolls():
+
+    def d6(x):
+        total = 0
+        for i in range(x):
+            total += random.randint(1, 6)
+        return total
+
+    def d10(x):
+        total = 0
+        for i in range(x):
+            total += random.randint(1, 10)
+        return total
+
+    def d20(x):
+        total = 0
+        for i in range(x):
+            total += random.randint(1, 20)
+        return total
 
 
 
-class Character():
+class char_stuff():
 
-    def __init__(self, name, hp, inventory, mdef, dex, spd):
-        self.name = name
-        self.hp = hp
-        self.inventory = inventory
-        self.mdef = mdef
-        self.dex = dex
-        self.spd = spd
+    def __init__(self):
+        self.name = char_stuff.get_charName()
+        self.inventory = []
+        self.hp = int(dice_rolls.d6(4)) + 15
+        self.defence = dice_rolls.d6(4)
+        self.strength = dice_rolls.d6(4)
+        self.speed = dice_rolls.d6(4)
+
+    def npc_maker(self, name, hp, inventory, mdef, dex, spd):
+        pass
+
+    def __str__(self):
+        os.system('cls' if os.name == 'nt' else 'clear')
+        stats = f"************  {self.name}  ************\n"
+        stats += f"\n| -  HP =  {self.hp}\n"
+        stats += f"| -  DEFENCE =  {self.defence}\n"
+        stats += f"| -  STRENGTH =  {self.strength}\n"
+        stats += f"| -  SPEED =  {self.speed}\n"
+        stats += f"| - INVENTORY : "
+        for i in self.inventory:
+            stats += f"{i}, "
+        stats += "\n"
+        stars = len(self.name) * "*"
+        stats += f"\n**************{stars}**************"
+        return stats
+
+    def get_charName():
+        return input("Enter your character's name : ")
 
 
-someguy = Character('bloke', 30, ['Iron Dagger', 'Leather Armour'], 5, 3, 7)
+    def add_item(self, item):
+        self.inventory.append(item)
 
-print(someguy.inventory)
+    def drop_item(self): # im having a hard time getting this to work, hopefully i can get it fixed pretty quick lol
+        
+        item = input("Which item do you want to drop? : ")
+
+        try:
+            item = input("Which item do you want to drop? : ")
+            for i in self.inventory:
+                if i == item:
+                    self.inventory.remove(item)
+                    print("Item removed: ", item)
+            return
+        except IndexError:
+            print("You can only drop items currently in your inventory.")
+            self.drop_item()
+
+
+
+
+
+
+someguy = char_stuff()
+print(someguy)
+someguy.add_item("my penis")
+someguy.add_item("my balls")
+someguy.drop_item()
+print(someguy)
+k = input("lol")
